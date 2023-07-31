3d9f4718204f7b543da722734c8152d4b81d053a
Patricia Martins <patricia_martins012@hotmail.com>
2023-07-31 19:31:50 -0400

    Adding the card slider of products page

--- js/main.js
+++ js/main.js
@@ -1,39 +1,29 @@
 console.log("It is working!");
 
-function checkAge() {
-  const birthYearInput = document.getElementById("birthYear");
-  const birthYear = birthYearInput.value;
-  const currentYear = new Date().getFullYear();
 
-  if (currentYear - birthYear >= 21) {
-    document.getElementById("ageModal").style.display = "none";
-  } else {
-    window.location.href = "https://www.wise-drinking.com/";
-  }
+// Hero slider from Products Page
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i,
+        slides = document.getElementsByClassName("mySlides"),
+        dots = document.getElementsByClassName("dot");
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    setTimeout(showSlides, 4000); // Change image every 2 seconds
 }
 
-// Function to check if the modal has been shown before
-function hasModalBeenShown() {
-  return localStorage.getItem("modalShown") === "true";
-}
-
-// Function to set the flag in local storage indicating the modal has been shown
-function setModalShown() {
-  localStorage.setItem("modalShown", "true");
-}
-
-window.onload = function () {
-  const ageModal = document.getElementById("ageModal");
-
-  // Check if the modal has been shown before, if not, show it
-  if (!hasModalBeenShown()) {
-    ageModal.style.display = "block";
-
-    // Setup the close button to hide the modal and set the flag when closed
-    const closeButton = ageModal.querySelector("button");
-    closeButton.addEventListener("click", function () {
-      ageModal.style.display = "none";
-      setModalShown();
-    });
-  }
-};
+// End of Hero slider from Products Page
